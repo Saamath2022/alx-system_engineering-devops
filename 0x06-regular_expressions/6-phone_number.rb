f574022b0cba1ad71af2b2d4d292987f86c9f46e
Saamath2022 <saaaondow@gmail.com>
2024-05-14 21:29:36 -0700

    6-phone_number.rb

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+# Get the argument from the command line
+phone_number = ARGV[0]
+
+# Define the regular expression pattern for a 10-digit phone number
+phone_regex = /^\d{10}$/
+
+# Check if the provided phone number matches the regular expression
+if phone_number.match?(phone_regex)
+  puts phone_number
+else
+  puts "Phone number does not match the required format."
+end
+
