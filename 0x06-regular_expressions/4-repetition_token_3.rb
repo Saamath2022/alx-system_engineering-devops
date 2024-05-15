7cca2c6fb1563a1675b553dbae04c03d0564c42e
Saamath2022 <saaaondow@gmail.com>
2024-05-14 21:17:49 -0700

    4-repetition_token_3.rb

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -4,7 +4,6 @@
 input_string = ARGV[0]
 
 # Match the input string against the regular expression
-match_result = input_string.scan(/hb(on|tn|t+n)/)
+match_result = input_string.scan(/hbt*n/)
 
-# Print the match result
 puts match_result.join("\n")
