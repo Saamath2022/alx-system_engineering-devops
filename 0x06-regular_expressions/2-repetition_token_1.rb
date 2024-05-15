8e28af0c272fb05bf0f0d511f0056237082aca6c
Saamath2022 <saaaondow@gmail.com>
2024-05-14 20:07:50 -0700

    2-repetition_token_1.rb

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+# Get the argument from the command line
+input_string = ARGV[0]
+
+# Match the input string against the regular expression
+match_result = input_string.scan(/hb*t*n/).join
+
+# Print the match result
+puts match_result
