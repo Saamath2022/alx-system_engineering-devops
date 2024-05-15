84f7b1362c79481faca0575eb98e3263e50e2df0
Saamath2022 <saaaondow@gmail.com>
2024-05-14 21:07:06 -0700

    4-repetition_token_3.rb

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -4,8 +4,7 @@
 input_string = ARGV[0]
 
 # Match the input string against the regular expression
-match_result = input_string.scan(/hb[ot]+`:wqin/)
+match_result = input_string.scan(/hb(on|tn|t+n)/)
 
 # Print the match result
 puts match_result.join("\n")
-
