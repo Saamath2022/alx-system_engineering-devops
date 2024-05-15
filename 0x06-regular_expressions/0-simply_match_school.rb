f052839c0beb2ac6bb4746b3289b033cd8210b7a
Saamath2022 <saaaondow@gmail.com>
2024-05-14 19:42:18 -0700

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,3 @@
 #!/usr/bin/env ruby
 puts ARGV[0].scan(/School/).join
 
-# Example Ruby code with Oniguruma regular expression
-text = "Hello, World! This is an example text."
-regex = /Hello, (\w+)!/
-
-# Match the regular expression against the text
-match_data = text.match(regex)
-
-# Check if there was a match
-if match_data
-  # Print the matched part
-  puts "Matched: #{match_data[0]}"
-
-  # Print the captured group
-  puts "Captured group: #{match_data[1]}"
-else
-  puts "No match found."
-end
